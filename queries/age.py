10a097cf8ca3c4754504dffe4c5fdb07c3540baa
Andrea Ignacio <robertodieguezgt@gmail.com>
2020-05-20 22:39:23 -0600

    added age queries and fixed states queries

--- queries/age.py
+++ queries/age.py
@@ -7,24 +7,37 @@ import pandas as pd
 from elastic.connection import elastic_instance
 from elastic.credentials import cloud_id, user, password
 
-def get_by_state(state):
-    #Connecting to elasticsearch
+def get_by_age(age):
     es = elastic_instance(cloud_id, user, password)
 
     query = {
         "size": 10000,
         "query": {
             "match" : {
-                "state" : state
+                "age" : age
             }
         }
     }
-
     response = es.search(index = "accounts", body = query )
     dataframe = pd.DataFrame([account["_source"] for account in response["hits"]["hits"]])
     return dataframe
 
-def get_by_states(states):
-    return pd.concat([get_by_state(state) for state in states]).set_index("account_number").sort_values(by = ["account_number"])
+def get_by_age_range(start, end): #start is inclusive end is exclusive
+    es = elastic_instance(cloud_id, user, password)
+    query = {
+    "size": 10000,
+    "query": {
+        "range" : {
+            "age" : {
+                "gte" : start,
+                "lt" : end
+                }
+            }
+        }
+    }
+    response = es.search(index = "accounts", body = query )
+    dataframe = pd.DataFrame([account["_source"] for account in response["hits"]["hits"]])
+    return dataframe
 
-print(get_by_states(["FL", "ID","CA"]))
+
+print(get_by_age_range(20,32))
